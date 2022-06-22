26ac6b256a827eab3e72ccc73c58142fb81f91bf
Lee GoEun <goeunlee@hansol.com>
2022-06-22 10:29:52 +0900

    타임리프 적용

--- src/main/java/com/hansol/handa/controller/ChallengeController.java
+++ src/main/java/com/hansol/handa/controller/ChallengeController.java
@@ -1,14 +1,39 @@
 package com.hansol.handa.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ChallengeController {
 	
 	@GetMapping("/test")
 	public String test() {
 		return "Hello World";
+	}
+	
+	@GetMapping({"/", "/list"})
+	public String list() {
+		System.out.println("index---------------------------------------");
+		return "index";
+	}
+	
+	@GetMapping("/create")
+	public String create() {
+		System.out.println("create---------------------------------------");
+		return "create";
+	}
+	
+	@GetMapping("/detail")
+	public String detail() {
+		System.out.println("detail---------------------------------------");
+		return "detail";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		System.out.println("login---------------------------------------");
+		return "login";
 	}
 	
 }
