79c12fa29f20fe92a7115118ae24e4c54dcc2e58
HyeonJeong Jeon <hyeonjeong.jeon@hansol.com>
2022-06-23 10:59:53 +0900

    마이페이지 내 정보 화면 초안

--- src/main/java/com/hansol/handa/controller/MypageController.java
+++ src/main/java/com/hansol/handa/controller/MypageController.java
@@ -1,17 +1,27 @@
 package com.hansol.handa.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ch.qos.logback.classic.Logger;
+
 @Controller
 @RequestMapping("mypage")
 public class MypageController {
+	private final Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
 	
-	@GetMapping("memberdetail")
+	@GetMapping("/memberdetail")
 	public String memberDatail() {
-		System.out.println("memberDetail-------------------------------");
-		return "/mypage/memberDetail";
+		logger.info("memberDetail-------------------------------");
+		return "mypage/memberDetail";
+	}
+	
+	@GetMapping("/challenge-produce")
+	public String challengeProduce() {
+		logger.info("challenge-produce-------------------------");
+		return "mypage/challengeProduce";
 	}
 	 
 }
