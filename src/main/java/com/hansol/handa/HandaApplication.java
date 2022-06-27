8257848154338f29314d5f56158397a902e998a9
HyeonJeong Jeon <hyeonjeong.jeon@hansol.com>
2022-06-27 13:21:32 +0900

    chore : 서버 배포 방식 변경 기존에 jar 파일로 배포하던 방식을 프로젝트 디렉토리를 볼 수 있도록 war 파일로 배포하는 방식으로 변경

--- src/main/java/com/hansol/handa/HandaApplication.java
+++ src/main/java/com/hansol/handa/HandaApplication.java
@@ -2,9 +2,18 @@ package com.hansol.handa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HandaApplication {
+public class HandaApplication extends SpringBootServletInitializer{
+
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		// TODO Auto-generated method stub
+		return application.sources(HandaApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HandaApplication.class, args);
