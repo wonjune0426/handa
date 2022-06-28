58a919bc921d1be4377e74879ef867da21e6ff76
HyeonJeong Jeon <hyeonjeong.jeon@hansol.com>
2022-06-28 16:50:03 +0900

    style : css 파일 변경 화면 구조 변경으로 인한 css 파일 변경

--- src/main/java/com/hansol/handa/controller/ChallengeController.java
+++ src/main/java/com/hansol/handa/controller/ChallengeController.java
@@ -28,32 +28,67 @@ public class ChallengeController {
 	}
 	
 	
-	@GetMapping("/") 
+	@GetMapping("/")
 	// Model의 addAttribute 함수 사용이 안되어 ModelMap 사용
-	// 맨 처음 화면은 전체 챌린지 리스트 조회
+	// 맨 처음 화면은 전체 챌린지 리스트 최신 순 조회 
 	public String list(ModelMap model) throws Exception{ 
 		List<ChallengeVO> challengeList = challengeService.selectAllChallenge();
 		model.addAttribute("challengeList", challengeList);
 	  
 		return "challenge/list"; 
 	}
-	 
+
+	/**
+	 * @param category : 카테고리 ID (1 ~ 6)
+	 * @param sortType : 정렬 타입 (0: 최신 순, 1: 오래된 순, 2: 참여 인원 순)
+	 * @return : 챌린지 리스트
+	 */
 	
 	@GetMapping("/list")
-	// 각 카테고리 별 챌린지 리스트 조회
+	// 전체 리스트 정렬 & 각 카테고리 별 챌린지 리스트 조회, 정렬
 	public String list(@RequestParam(required = false) String category, @RequestParam(required = false) String sortType,
 				ModelMap model) throws Exception{
 		
-		//logger.info(category + " " + sortType);
-		int categoryID = Integer.parseInt(category);
+		List<ChallengeVO> challengeList = null;
+		Boolean isCategory = true;	// 카테고리 리스트 조회 인지 아닌지
 		
-		List<ChallengeVO> challengeList = challengeService.selectChallengeList(categoryID);
-		Map<String, String> categoryName = challengeService.selectCategoryName(categoryID);
-		
+		if(category == null) {
+			isCategory = false;	// 전체 리스트 조회일 경우
+			
+			switch(sortType) {
+				case "0":	// 최신 순
+					challengeList = challengeService.selectAllChallenge();
+					break;
+				case "1":	// 오래된 순
+					challengeList = challengeService.selectAllChallengeDesc();
+					break;
+				case "2":	// 참여 인원 순
+					break;
+			}
+		}
+		else {
+			int categoryID = Integer.parseInt(category);
+			Map<String, String> categoryName = challengeService.selectCategoryName(categoryID);
+			
+			switch(sortType) {
+				case "0":	// 최신 순
+					challengeList = challengeService.selectChallengeList(categoryID);
+					break;
+				case "1": 	// 오래된 순
+					challengeList = challengeService.selectChallengeListDesc(categoryID);
+					break;
+				case "2":	// 참여 인원 순
+					break;
+			}
+			
+			model.addAttribute("categoryID", categoryID);
+			model.addAttribute("subCategoryName", categoryName.get("sub_category_name"));
+			model.addAttribute("mainCategoryName", categoryName.get("main_category_name"));
+		}
+
 		model.addAttribute("challengeList", challengeList);
-		model.addAttribute("subCategoryName", categoryName.get("sub_category_name"));
-		model.addAttribute("mainCategoryName", categoryName.get("main_category_name"));
-		
+		model.addAttribute("isCategory", isCategory);
+
 		return "challenge/list";
 	}
 	
--- src/main/java/com/hansol/handa/mapper/ChallengeMapper.java
+++ src/main/java/com/hansol/handa/mapper/ChallengeMapper.java
@@ -11,6 +11,8 @@ import com.hansol.handa.domain.ChallengeVO;
 public interface ChallengeMapper {
 	
 	List<ChallengeVO> selectAllChallenge();
+	List<ChallengeVO> selectAllChallengeDesc();
 	List<ChallengeVO> selectChallengeList(int category_id);
+	List<ChallengeVO> selectChallengeListDesc(int category_id);
 	Map<String, String> selectCategoryName(int sub_category_id);
 }
