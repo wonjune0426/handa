ddc9579beea7dfad471b8c72ad8d722f9c34ef5d
HyeonJeong Jeon <hyeonjeong.jeon@hansol.com>
2022-07-01 09:47:34 +0900

    style : 챌린지 리스트 화면 수정 [소분류 이름] 리스트 이름으로 변경 챌린지 리스트 card 구성 변경 챌린지 상태 버튼 추가, 현재 날짜에 따라 챌린지 상태 반영

--- src/main/resources/static/assets/js/infiniteScroll.js
+++ src/main/resources/static/assets/js/infiniteScroll.js
@@ -32,3 +32,40 @@ const fetchItem = () => {
   itemIndex += 2
 }
 */
+
+// onload는 페이지의 모든 리소스가 로드 완료되었을 때 실행된다.
+/*
+window.onload = () => {
+    const observerOption = {
+        root: null,
+        rootMargin: "0px 0px 30px 0px",
+        threshold: 0.2
+    }
+    // IntersectionObserver 인스턴스 생성
+    const io = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+			// entry와 observer 출력
+			console.log('entry:', entry);
+    		console.log('observer:', observer);
+            // entry.isIntersecting: 특정 요소가 뷰포트와 20%(threshold 0.2) 교차되었으면
+            if (entry.isIntersecting) {
+                entry.target.src = entry.target.dataset.src
+                observer.unobserve(entry.target)    // entry.target에 대해 관찰 종료
+            }
+        })
+    }, observerOption)
+    // lazy-img 클래스 요소 순회
+    const lazyImgs = document.querySelectorAll(".card")
+    lazyImgs.forEach(el => {
+        io.observe(el)  // el에 대하여 관측 시작
+    })
+}
+*/
+
+const $cards = document.querySelectorAll('.card')
+
+const observer = new IntersectionObserver(entries => {
+	console.log(entries)
+})
+// 1번째 요소 사라지거나 다시 나타날 때 객체 배열 출력
+observer.observe($cards[0])
