6d1adbe71d70f26620ff2b186594dd6f3be4eedd
HyeonJeong Jeon <hyeonjeong.jeon@hansol.com>
2022-07-01 13:30:45 +0900

    refactor : 챌린지 리스트 조최 수정 url 변경 챌린지 리스트 조회 쿼리문 수정

--- src/main/java/com/hansol/handa/controller/ChallengeController.java
+++ src/main/java/com/hansol/handa/controller/ChallengeController.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -36,19 +37,19 @@ public class ChallengeController {
 	}
 	
 	
-	@GetMapping("/")
-	// Model의 addAttribute 함수 사용이 안되어 ModelMap 사용
-	// 맨 처음 화면은 전체 챌린지 리스트 최신 순 조회 
-	public String list(ModelMap model) throws Exception{ 
-		List<ChallengeVO> challengeList = challengeService.selectAllChallenge();
-		
-		challengeList = getChallengeState(challengeList);
-		
-		model.addAttribute("challengeList", challengeList);
-		model.addAttribute("sortType", 0);
-		
-		return "challenge/list"; 
-	}
+	/* 메인 페이지 넣을 곳! 삭제해도 괜찮아요!
+	 * @GetMapping("/") // Model의 addAttribute 함수 사용이 안되어 ModelMap 사용 // 맨 처음 화면은 전체
+	 * 챌린지 리스트 최신 순 조회 public String list(ModelMap model) throws Exception{
+	 * List<ChallengeVO> challengeList =
+	 * challengeService.selectChallegeListCategory(null, null);
+	 * 
+	 * //challengeList = getChallengeState(challengeList);
+	 * 
+	 * model.addAttribute("challengeList", challengeList);
+	 * model.addAttribute("sortType", 0);
+	 * 
+	 * return "challenge/list"; }
+	 */
 
 	/**
 	 * @param category : 카테고리 ID (1 ~ 6)
@@ -60,84 +61,28 @@ public class ChallengeController {
 	// 전체 리스트 정렬 & 각 카테고리 별 챌린지 리스트 조회, 정렬
 	public String list(@RequestParam(required = false) String category, @RequestParam(required = false) String sortType,
 				ModelMap model) throws Exception{
+
+		Boolean isCategory = true;
 		
-		List<ChallengeVO> challengeList = null;
-		Boolean isCategory = true;	// 카테고리 리스트 조회 인지 아닌지
+		if(sortType == null) sortType = "0";
 		
-		if(category == null) {
-			isCategory = false;	// 전체 리스트 조회일 경우
-			
-			switch(sortType) {
-				case "0":	// 최신 순
-					challengeList = challengeService.selectAllChallenge();
-					break;
-				case "1":	// 오래된 순
-					challengeList = challengeService.selectAllChallengeDesc();
-					break;
-				case "2":	// 참여 인원 순
-					challengeList = challengeService.selectAllChallengeJoin();
-					break;
-			}
-		}
-		else {	// 카테고리 별 리스트 조회일 경우
-			int categoryID = Integer.parseInt(category);
+		if(category == null) isCategory = false;
+		else{
+			int categoryID = Integer.parseInt(category);  
 			Map<String, String> categoryName = challengeService.selectCategoryName(categoryID);
-			
-			switch(sortType) {
-				case "0":	// 최신 순
-					challengeList = challengeService.selectChallengeList(categoryID);
-					break;
-				case "1": 	// 오래된 순
-					challengeList = challengeService.selectChallengeListDesc(categoryID);
-					break;
-				case "2":	// 참여 인원 순
-					challengeList = challengeService.selectChallengeListJoin(categoryID);
-					break;
-			}
 			
 			model.addAttribute("categoryID", categoryID);
 			model.addAttribute("subCategoryName", categoryName.get("sub_category_name"));
 			model.addAttribute("mainCategoryName", categoryName.get("main_category_name"));
 		}
 		
-		challengeList = getChallengeState(challengeList);
-		
-		model.addAttribute("challengeList", challengeList);
-		model.addAttribute("isCategory", isCategory);
-		model.addAttribute("sortType", sortType);
+		List<ChallengeVO> challengeList = challengeService.selectChallegeList(category, sortType);
 
+		model.addAttribute("challengeList", challengeList);
+		model.addAttribute("isCategory", isCategory); 
+		model.addAttribute("sortType", sortType);
 		
 		return "challenge/list";
-	}
-	
-	// 현재 날짜 기준으로 챌린지 상태 설정
-	public List<ChallengeVO> getChallengeState(List<ChallengeVO> list) {
-		
-		//'yyyy-MM-dd' 포맷 설정
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		
-		//오늘날짜
-		LocalDate today = LocalDate.now();
-		
-		for(int i = 0; i < list.size(); i++) {
-			ChallengeVO vo = list.get(i);
-			
-			//비교할 date를 데이터 포맷으로 변경
-			LocalDate startDate = LocalDate.parse(vo.getStartdate(), formatter);
-			LocalDate endDate = LocalDate.parse(vo.getEnddate(), formatter);
-			
-			// 현재 날짜가 시작 날짜 전이면 모집중
-			if(today.isBefore(startDate))
-				list.get(i).setChallenge_state("모집중");
-			// 현재 날짜가 종료 날짜 이후면 마감
-			else if(today.isAfter(endDate))
-				list.get(i).setChallenge_state("마감");
-			// 현재 날짜가 시작 날짜와 종료 날짜 사이면 진행중
-			else
-				list.get(i).setChallenge_state("진행중");
-		}
-		
-		return list;
 	}
 	
 	@GetMapping("/imagelist/{searchWord}")
