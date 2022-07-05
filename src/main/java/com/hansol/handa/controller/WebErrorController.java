6142c5ef1661e6c7510aee7267832afedea365fa
Lee GoEun <goeunlee@hansol.com>
2022-07-05 17:08:39 +0900

    feat : 에러페이지 수정
    error/error.html 페이지에서 모든 에러 보여주도록 수정

--- src/main/java/com/hansol/handa/controller/WebErrorController.java
+++ src/main/java/com/hansol/handa/controller/WebErrorController.java
@@ -6,26 +6,38 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebErrorController implements ErrorController {
 
 	@GetMapping("/error") 
-	public String handleError(HttpServletRequest request) {
+	public String handleError(HttpServletRequest request, Model model) {
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
 		if (status != null) {
 			int statusCode = Integer.valueOf(status.toString());
 
 			if (statusCode == HttpStatus.NOT_FOUND.value()) {
-				return "error/404error";
+				
+				model.addAttribute("code", HttpStatus.NOT_FOUND.value());
+				model.addAttribute("msg", "URL을 확인해주세요.");
+				
+				return "error/error";
 			}
 
 			if (statusCode == HttpStatus.FORBIDDEN.value()) {
-				return "error/403error";
+				
+				model.addAttribute("code", HttpStatus.FORBIDDEN.value());
+				model.addAttribute("msg", "허용되지 않는 접근입니다.");
+				
+				return "error/error";
 			}
 		}
+		
+		model.addAttribute("code", "ERROR");
+		model.addAttribute("msg", "오류");
 
 		return "error/error";
 	}
