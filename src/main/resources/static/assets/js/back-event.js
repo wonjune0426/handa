8d1a0dc7f3431700d9e8d825f5a89b6aa11394d7
HyeonJeong Jeon <hyeonjeong.jeon@hansol.com>
2022-07-15 09:29:11 +0900

    refactor : 메인페이지 인기 챌린지 수정 인기 챌린지 유료 아이콘 추가

--- src/main/resources/static/assets/js/back-event.js
+++ src/main/resources/static/assets/js/back-event.js
@@ -1,11 +1,16 @@
 /**
  * 
  */
- 
+
 window.onpageshow = function(event) {
 	if ( event.persisted || (window.performance && window.performance.navigation.type == 2)) {
-		// 이전 페이지의 url
-		var referrer = document.referrer;
-		window.location.href = referrer;
+		//이전 페이지의 url
+		//var referrer = document.referrer;
+		//window.location.href = referrer;
+		//console.log(LogcalStorage.getItem('sort'));
+		
+		//localStorage.setItem("sort", '1');
+		//console.log(referrer);
+		//document.
 	}
-}
+};
