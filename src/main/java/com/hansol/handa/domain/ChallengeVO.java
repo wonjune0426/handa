be090b3c0654285fb082b2e854add42e0436b3d6
최 원준 <wonjunechoi@hansol.com>
2022-07-22 16:37:39 +0900

    refactor:마감인원관련기능
    챌린지 생성,수정 시에 마감인원 추가

--- src/main/java/com/hansol/handa/domain/ChallengeVO.java
+++ src/main/java/com/hansol/handa/domain/ChallengeVO.java
@@ -1,6 +1,5 @@
 package com.hansol.handa.domain;
 
-import java.time.LocalDate;
 
 import lombok.Data;
 
@@ -16,6 +15,7 @@ public class ChallengeVO {
 	 private String createdate;
 	 private String member_id; 
 	 private int subcategory_id;
+	 private int limit_member;
 	 private String challenge_type;
 	 private String challenge_state;
 	 private String challenge_cost;
